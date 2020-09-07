2160456d8752d1aeea5246bb862e7f2e7973cfba
fatpav <fat_pav@hotmail.com>
2020-09-07 18:51:37 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,10 @@
+number_1 = 50
+print(number_1)
+number_2 = 25
+result = number_1 * number_2
+print(result)
+name = "Paul Donegan"
+question = "How are you today?"
+print("Hello " +name+ ", it's good to see you.  " +question+ "")
+result_2 = question * number_2
+print(result_2)
